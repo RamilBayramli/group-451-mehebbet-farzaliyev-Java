378a492ba3667cafc588c834d708beb9d4a6528d
RamilBayramli <r93149637@gmail.com>
2026-02-12 20:13:03 +0400

    Update Classwork_1.java

--- src/classwork_1/Classwork_1.java
+++ src/classwork_1/Classwork_1.java
@@ -1,10 +1,103 @@
 public class Main {
     public static void main(String[] args) {
- for(int i = 1; i <= 8 i++) {
-	 for (int a = 8; s > i; a
-			)
- }
+    	//1
+    	int num = sc.nextInt();
+    	if (num % 2 == 0) {
+    	    System.out.println("Четное");
+    	} else {
+    	    System.out.println("Нечетное");
+    	}}
+    //2
+    if (num > 0) {
+        System.out.println("Полож");
+    } else if (num < 0) {
+        System.out.println("Отриц");
+    } else {
+        System.out.println("Ноль");
     }
+    //3
+    for (int i = 1; i <= 10; i++) {
+        System.out.println("5 x " + i + " = " + (5 * i));
     }
+    //4
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    System.out.println(sum);
+}
+  //5
+?
+		//6
+		int count = 0;
+for (int i = 0; i < 10; i++) {
+    int num = sc.nextInt();
+    if (num % 2 == 0) {
+        count++;
+        System.out.println(count);
+    }
+}
+//7
+int A = sc.nextInt();
+int factorial = 1;
+for (int I = 1; i <= n; I++) {
+    factorial *= I;
+    System.out.println(factorial);
+}
+//8
+int Max = sc.nextInt();
+
+for (int I = 1; i < 5; i++) {
+    int num = sc.nextInt();
+    if (num > max) {
+        max = num;
+        System.out.println(max);
+    }
+}
+//9
+int s = sc.nextInt();
+int r = sc.nextInt();
+
+for (int i = 0; i < s; i++) {
+    for (int j = 0; j < r; j++) {
+        System.out.print("*");
+    }
+    //10
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    //11
+    int L = sc.nextInt();
+    int sum = 0;
+
+    for (int i = 2; i <= L; i += 2) {
+        sum += i;
+    }
+    System.out.println(sum);
+    //12
+    
+}
+}
+
+
+}
+}
+
+
+}
+}
+}
+
+
+
+
+
+
+
+
+
 
    
