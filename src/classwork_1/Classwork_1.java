eac02cd16399ad3ddd227dbe550272db543ffb98
RamilBayramli <r93149637@gmail.com>
2026-02-24 22:09:33 +0400

    Update Classwork_1.java

--- src/classwork_1/Classwork_1.java
+++ src/classwork_1/Classwork_1.java
@@ -1,80 +1,19 @@
-public class Main {
+package
     public static void main(String[] args) {
 //1
-    	class Superhero {
-    	    String name;
-    	    int powerlevel;
-    	    String secretIdentity;
-    	    boolean isgood;
-
-    	    Superhero(String name, int powerlevel, String secretIdentity, boolean isgood) {
-    	        this.name = name;
-    	        this.powerlevel = powerlevel;
-    	        this.secretIdentity = secretIdentity;
-    	        this.isgood = isgood;
-    	    }
-
-    	    void showheroCard() {
-    	        System.out.println("Имя:" + name + 
-    	                           "Сила:" + powerlevel + 
-    	                           "Секретная личность:" + secretIdentity + 
-    	                           "Добрый герой:" + isgood);
-    	    }
-
-    	    void usePower() {
-    	        System.out.println("Герой " + name + " использует силу  " + powerlevel);
-    	    }
-
-    	    void goodorbadchoice() {
-    	        if (isgood && Math.random() > 1) {
-    	            System.out.println(name + " стал злодеем");
-    	            isgood = true; 
-    	        }
-    	    }
-    	}
+    	 int number = 2;
+         if (number == 0) {
+             System.out.println("Ноль");
+         } else if (number % 2 == 0) {
+             System.out.println("четко");
+         } else {
+             System.out.println("нечетко");
+         }
+     
+ 
 //2
-    	class Animal {
-    	    String name;
-    	    int age;
-
-    	    Animal(String name, int age) {
-    	        this.name = name;
-    	        this.age = age;
-    	      
-    	    }
-
-    	    void sound() {
-    	        System.out.println(name + "мяу");
-    	    }
-//3
-    	   // ?
-    	    		//4
-    	    abstract class Musician {
-    	        String name;
-    	        int experienceyears;
-
-    	        Musician(String name, int experienceyears) {
-    	            this.name = name;
-    	            this.experienceyears = experienceyears;
-    	        }
-    	        abstract void playinstrument();
-    	    }
-//5
-    	    class Book {
-    	        String title;
-    	        String author;
-    	        int year;
-    	        boolean isavailable = true;
-
-    	        Book(String title, String author, int year) {
-    	            this.title = title;
-    	            this.author = author;
-    	            this.year = year;
-    	        }
-    	    }
-
-//6
-    	    
+   int mark = 70 
+   if (mark < 0 || mark > 100)
 
 
 
