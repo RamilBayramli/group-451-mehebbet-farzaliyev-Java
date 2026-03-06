a41896354186fda686fadd6ff5037ee82cb15b89
RamilBayramli <r93149637@gmail.com>
2026-03-06 22:13:50 +0400

    Create classwork_3.java

---
+++ src/classwork_3/classwork_3.java
@@ -0,0 +1,19 @@
+package classwork_3;
+public class Runner {
+public static void main(String[] args) {
+	//1
+ int sum(int a, int b) {
+	return a + b
+}
+//2
+ isEven(int number) {
+	 return number % 2 == 0;
+ }
+//3
+ for(int i = 1; i<= 10; i++) {
+	 System.out.println(i);
+ }
+ //4
+ 
+}
+}
