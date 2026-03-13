a9a5560207da917a070827666e99db3f0b661e8a
RamilBayramli <r93149637@gmail.com>
2026-03-13 21:51:15 +0400

    Create package-info.java

---
+++ src/classwork_3/package-info.java
@@ -0,0 +1,32 @@
+package classwork_3;
+
+public class Runner {
+	public static void main(String[] args) {
+		//1
+		 if (num % 2 == 0)
+	            System.out.println("чет");
+	        else
+	            System.out.println("нечет");
+		 //2
+		 int sum = 0;
+	        for (int i = 1; i <= a; i++) {
+	            sum += i;
+	        }
+	        System.out.println(sum);
+	        //3
+	        int num = 0
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println((num * i));
+	        }
+	        //4
+	        int num = 0
+	        for (int i = 1; i < 5; i++) {
+	           if  (num > max) max = num;
+	        }
+	        System.out.println(max);
+	//5
+	        //?
+
+
+	}
+}
