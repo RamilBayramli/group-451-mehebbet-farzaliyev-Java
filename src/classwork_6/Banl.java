5f7ad8e285af1c109793ed82bfb56982724db8f2
RamilBayramli <r93149637@gmail.com>
2026-04-21 17:08:32 +0400

    Create Banl.java

---
+++ src/classwork_6/Banl.java
@@ -0,0 +1,11 @@
+package classwork_6;
+public class Banl {
+	    public String owner;
+	    public double balance;
+	    public Banl(String owner, double balance) {
+	    this.owner = owner;
+	    this.balance = balance;
+	
+}
+}
+
