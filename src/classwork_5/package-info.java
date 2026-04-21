b3a453cc990e793fc5b73a4b5863af89ef629f0d
RamilBayramli <r93149637@gmail.com>
2026-04-21 17:08:21 +0400

    Create package-info.java

---
+++ src/classwork_5/package-info.java
@@ -0,0 +1,29 @@
+package classwork_5;
+public class Runner {
+    public static void main(String[] args) {
+    	//1
+    	 Car car = new Car();
+         car.brand = "BMW";
+         car.model = "x5";
+         car.year = 2017;
+       System.out.println(car.brand  + car.model+ car.year
+);
+    }
+    }
+//2
+Student student = new Student();
+student.name = "Рамиль";
+student.age = "13";
+student.grade = 5;
+}
+}
+//3
+int width, height;
+	Rectangle(int widht, int height) {
+		this.width = width;
+		this.height = height;
+}
+int getarea() {
+	return width * height;
+}
+
