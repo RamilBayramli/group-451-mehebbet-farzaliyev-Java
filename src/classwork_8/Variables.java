e8c8a923269f4ea8a09b07d215a401905db33f1d
RamilBayramli <r93149637@gmail.com>
2026-04-28 21:58:44 +0400

    Create Variables.java

---
+++ src/classwork_8/Variables.java
@@ -0,0 +1,8 @@
+package classwork_8;
+public class Variables {
+	 public static void main(String[] args) {
+		 int age = 13; 
+		 int future = age + 10;
+		 System.out.println("через 10 лет мне будет " + future);
+ }
+}
