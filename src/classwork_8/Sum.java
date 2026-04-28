5f82c2d07614909c7cd0034bd46f7ce319bb8aa6
RamilBayramli <r93149637@gmail.com>
2026-04-28 21:58:47 +0400

    Create Sum.java

---
+++ src/classwork_8/Sum.java
@@ -0,0 +1,10 @@
+package classwork_8;
+public class Sum {
+	 public static void main(String[] args) {
+		 int sum = 0; 
+		 for (int i = 1; i <= 100; i++) {
+		     sum += i; 
+		 }
+		 System.out.println(sum); 
+	 }
+}
