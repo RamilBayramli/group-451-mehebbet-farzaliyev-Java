b726ddc461eb78bc9d3428c441764ef6a9c4ae79
RamilBayramli <r93149637@gmail.com>
2026-04-28 21:58:58 +0400

    Create Numbers.java

---
+++ src/classwork_8/Numbers.java
@@ -0,0 +1,8 @@
+package classwork_8;
+public class Numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
