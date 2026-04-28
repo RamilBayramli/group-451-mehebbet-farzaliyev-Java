b745651dc5808d482f726d8f538ea8ec80fbf263
RamilBayramli <r93149637@gmail.com>
2026-04-28 21:59:09 +0400

    Create Even.java

---
+++ src/classwork_8/Even.java
@@ -0,0 +1,11 @@
+package classwork_8;
+public class Even {
+	 public static void main(String[] args) {
+		 int n = 10; 
+		 if (n % 2 == 0) {
+		     System.out.println("чет");
+		 } else {
+		     System.out.println("нечет");
+		 }
+}
+}
