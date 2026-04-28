c5b3181da643a04a554cdafba2a2c4c4bf238a01
RamilBayramli <r93149637@gmail.com>
2026-04-28 21:59:03 +0400

    Create Multipliitian.java

---
+++ src/classwork_8/Multipliitian.java
@@ -0,0 +1,7 @@
+package classwork_8;
+public class Multipliitian {
+	 public static void main(String[] args) {
+		 for (int i = 1; i <= 10; i++) {
+			    System.out.println("3 * " + i + " = " + (3 * i));
+	 }
+}
