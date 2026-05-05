fe904a1d28b318fed9db7204a9ee27375a760e8f
RamilBayramli <r93149637@gmail.com>
2026-05-05 21:58:36 +0400

    Create Polojitchisla.java

---
+++ src/classwork_10/Polojitchisla.java
@@ -0,0 +1,13 @@
+package classwork_10;
+public class Polojitchisla {
+	public static void main(String[] args) {
+	        int sum = -1; 
+	        if (sum > 5) {
+	            System.out.println("положит");
+	        } else if (sum < 3) {
+	            System.out.println("отриц");
+	        } else {
+	            System.out.println("ноль");
+	        }
+	}
+}
