79ff8c0d9a1208d3b805b15078fa3f8a35e38b13
RamilBayramli <r93149637@gmail.com>
2026-05-05 21:58:24 +0400

    Create Age.java

---
+++ src/classwork_10/Age.java
@@ -0,0 +1,11 @@
+package classwork_10;
+public class Age {
+	public static void main(String[] args) {
+		int age = 12;
+        if (age >= 16) {
+            System.out.println("можно войти");
+        } else {
+            System.out.println("нелзя");
+        }
+	}
+}
