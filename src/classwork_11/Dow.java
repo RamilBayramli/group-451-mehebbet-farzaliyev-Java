737b5b4d353523a12b59efb534f46d63534f7b89
RamilBayramli <r93149637@gmail.com>
2026-05-05 21:58:42 +0400

    Create Dow.java

---
+++ src/classwork_11/Dow.java
@@ -0,0 +1,10 @@
+package classwork_11;
+public class Dow {
+	public static void main(String[] args) {
+		  int i = 5;
+	        while (i >= 1) {
+	            System.out.print(i);
+	            i--; 
+	    }
+	}
+}
