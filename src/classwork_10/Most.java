858ac29ee18d89dcd64f02eac4d64dd606e6faab
RamilBayramli <r93149637@gmail.com>
2026-05-05 21:58:32 +0400

    Create Most.java

---
+++ src/classwork_10/Most.java
@@ -0,0 +1,14 @@
+package classwork_10;
+public class Most {
+	public static void main(String[] args) {
+		 int a = 10;
+	        int b = 15;
+	        if (a > b) {
+	            System.out.println(a);
+	        } else if (b > a) {
+	            System.out.println(b);
+	        } else {
+	            System.out.println("числа равны");
+	        }
+	}
+}
