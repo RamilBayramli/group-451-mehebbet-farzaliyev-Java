d26ec4b4af8a21e98d460be967013d2184e88198
RamilBayramli <r93149637@gmail.com>
2026-05-05 21:58:56 +0400

    Create Multiplication.java

---
+++ src/classwork_11/Multiplication.java
@@ -0,0 +1,9 @@
+package classwork_11;
+public class Multiplication {
+	public static void main(String[] args) {
+		 int sum = 5;
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println(sum * i);
+	        }
+	}
+}
