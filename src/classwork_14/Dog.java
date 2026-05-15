e28235bd19ea6cb859b8a4a30f9489df2a055434
RamilBayramli <r93149637@gmail.com>
2026-05-15 21:23:33 +0400

    Create Dog.java

---
+++ src/classwork_14/Dog.java
@@ -0,0 +1,12 @@
+package classwork_14;
+public class Dog {
+	String name;
+	int age;
+	public Dog(String name, int age) {
+		this.name = name;
+	    this.age = age;	
+	}
+	public void bark() {
+		System.out.println("woff i am " + this.name);
+	}
+}
