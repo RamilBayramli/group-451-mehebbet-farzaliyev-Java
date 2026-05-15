395eaa456e99feebae96c6d2d2b13e695a5a101c
RamilBayramli <r93149637@gmail.com>
2026-05-15 21:23:36 +0400

    Create Rectangle.java

---
+++ src/classwork_14/Rectangle.java
@@ -0,0 +1,18 @@
+package classwork_14;
+
+public class Rectangle {
+	public int widht;
+	public int height;
+	 
+	public Rectangle(int widht, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	public Rectangle(int widht) {
+		this.width = width;
+		this.height = height;
+	}
+	public int area() {
+		return width * height;
+	
+}
